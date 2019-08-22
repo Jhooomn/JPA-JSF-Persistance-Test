7d931d25d82bb5f99696a5ba73d811b1548b8e8b
Jhon Baron <jhooomn@gmail.com>
2019-08-21 19:50:54 -0500

    Update ImplDao.java

--- src/java/com/dao/ImplDao.java
+++ src/java/com/dao/ImplDao.java
@@ -6,17 +6,12 @@
 package com.dao;
 
 import Controllers.FacesUtil;
-import java.io.Serializable;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Query;
-import javax.persistence.EntityNotFoundException;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import javax.persistence.PersistenceException;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 
 /**
  *
